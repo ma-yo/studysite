48a9f473d24a5c7cf4eb23f358b4712e23c252f6
ma-yo <ma-yo-2228@outlook.com>
2021-03-28 13:34:52 +0900

    javascript設定不具合修正

--- sansudrill/static/sansudrill/js/index.js
+++ sansudrill/static/sansudrill/js/index.js
@@ -1,14 +1,14 @@
 $(function(){
     $('input[name="drill_type"]').change(function () {
-        drillform_submit();
+        drillform_submit($(this).data('action'));
     });
 
     $('#id_create_drill').on('click', function() {
-        drillform_submit();
+        drillform_submit($(this).data('action'));
     });
 
-    function drillform_submit(){
-        $('#drill-form').attr("action", $(this).data('action'));
+    function drillform_submit(data_action){
+        $('#drill-form').attr("action", data_action);
         $('#drill-form').submit();
     }
 });
